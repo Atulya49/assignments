0911d8cc05002f20d3eec6dcf5538437d3972f18
Atulya49 <120500428+Atulya49@users.noreply.github.com>
2024-01-19 03:59:05 +0530

    given array of object return array of object Oject.keys(arr)

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,27 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+	var spend = {};
+  for(let i=0;i<transactions.length;i++){
+      var t=transactions[i];
+      if(spend[t.category])
+         spend[t.category]=spend[t.category]+t.price;
+      else
+        spend[t.category]=t.price;
+    }
+  console.log(spend);
+  var arr=Object.keys(spend)
+  var ans=[]
+  for(let i=0;i<arr.length;i++){
+    var category=arr[i];
+    var obj={
+      category: category,
+       totalSpent: spend[category]
+  }
+  }
+    ans.push(obj);
+  console.log(ans);
+  return ans;
 }
 
 module.exports = calculateTotalSpentByCategory;
