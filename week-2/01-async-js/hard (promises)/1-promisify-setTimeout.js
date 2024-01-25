3f418b9fb25c88ceb5a027bc6dd6772c4cbcbda7
Atulya49 <120500428+Atulya49@users.noreply.github.com>
2024-01-26 04:31:26 +0530

    basic promise kaise return hota hai-promisify-setTimeout.js

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,19 @@
 */
 
 function wait(n) {
+  return new Promise((p) => {
+    setTimeout(() => {
+      p(`Promise resolved after ${n} seconds`);
+    }, n * 1000);
+  });
 }
+
+wait(5)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
 module.exports = wait;
