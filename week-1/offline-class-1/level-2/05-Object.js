1472412fb7296467bbb1dadebcb249b97fc49006
Atulya49 <120500428+Atulya49@users.noreply.github.com>
2024-01-25 21:09:02 +0530

    aagya smjh 05-Object.js

--- week-1/offline-class-1/level-2/05-Object.js
+++ week-1/offline-class-1/level-2/05-Object.js
@@ -1,23 +1,25 @@
-// Object Methods Explanation
 function objectMethods(obj) {
   console.log("Original Object:", obj);
 
+  // key me iterate krne ke liye and retuens array of keys
   let keys = Object.keys(obj);
   console.log("After Object.keys():", keys);
 
+  // return values array
   let values = Object.values(obj);
   console.log("After Object.values():", values);
 
+  // return a array of array arr[i][0]=key and arr[i][1]=value
   let entries = Object.entries(obj);
   console.log("After Object.entries():", entries);
 
+  // if a property name property is there or not
   let hasProp = obj.hasOwnProperty("property");
   console.log("After hasOwnProperty():", hasProp);
 
+  // add new key value pair in object
   let newObj = Object.assign({}, obj, { newProperty: "newValue" });
   console.log("After Object.assign():", newObj);
-
-
 }
 
 // Example Usage for Object Methods
